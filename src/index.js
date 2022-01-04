f3d32b9b5cbdad918d4594ffd822aac9d6ab6159
ahmeddbahaa <ahmedbahaa0026@gmail.com>
2022-01-05 01:17:44 +0200

    Movie Component Table Configured

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import Movie from './components/movieComponent';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Movie />
   </React.StrictMode>,
   document.getElementById('root')
 );
